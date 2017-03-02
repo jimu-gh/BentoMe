739aed2d2631560025c4547aab5ecf98009b2b58
Kyle Strem <kylestremdev@gmail.com>
2017-03-01 17:50:20 -0800

    fun with admin forms

--- apps/adminbento/urls.py
+++ apps/adminbento/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^/adddish$', views.dish, name='dish'),
-    url(r'^/add$', views.add, name='dish')
+    url(r'^/add$', views.add, name='dish'),
+    url(r'^dummy$', views.dummy, name='dummy')
 ]
--- apps/adminbento/views.py
+++ apps/adminbento/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from ..home.models import *
+
+from .forms import *
 # Create your views here.
 def index(request):
     return render(request, 'adminbento/index.html')
@@ -9,3 +11,10 @@ def dish(request):
 
 def add(request):
     pass
+
+def dummy(request):
+    return render(request, 'adminbento/dummy.html',
+    {
+        'meal_form': MealForm(),
+        'dish_form': DishForm()
+    })
