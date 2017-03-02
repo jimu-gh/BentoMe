84466becaf327803ea3bd1bea8060687cd51bdc1
Kyle Strem <kylestremdev@gmail.com>
2017-03-01 16:05:44 -0800

    added superuser stuff and cleaned up user creation and show

--- bentome/urls.py
+++ bentome/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('apps.home.urls', namespace='home')),
     url(r'^users/', include('apps.users.urls', namespace='users')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^superadmin/', admin.site.urls),
 ]
