7d721fd5421c6c781334579615ec1f4dd69c092a
Kyle Strem <kylestremdev@gmail.com>
2017-03-03 12:50:51 -0800

    housekeeping

--- apps/adminbento/urls.py
+++ apps/adminbento/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     url(r'^create_dish/$', views.create_dish, name='create_dish'),
     url(r'^add_meal/$', views.add_meal, name="add_meal"),
     url(r'^create_meal/$', views.create_meal, name="create_meal"),
-<<<<<<< HEAD
     url(r'^login/$', views.login, name="login"),
     url(r'^logout/$', views.logout, name='logout'),
-=======
-    url(r'^menu/$', views.menu, name='menu'),
-    url(r'^logout/$', views.logout, name='logout')
->>>>>>> f96b9f4e7c84c9cc793925e25b83ff9d7c9b0437
 ]
