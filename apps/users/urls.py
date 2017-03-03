99e1552474afe84b2574358711e536fdeba52943
zer8ne <jimothy@gmail.com>
2017-03-03 10:50:45 -0800

    Message Form implemented

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^register/$', views.register, name="register"),
     url(r'^login/$', views.login, name="login"),
     url(r'^logout/$', views.logout, name="logout"),
-    url(r'^show/(?P<user_id>\d+?)/$', views.show, name="show")
+    url(r'^show/(?P<user_id>\d+?)/$', views.show, name="show"),
+    url(r'^create_feedback/$', views.create_feedback, name="create_feedback")
 ]
