9553a3bed4b89e2b0c31cccf0b10b356717e55b5
zer8ne <jimothy@gmail.com>
2017-03-12 20:04:37 -0700

    Update script.js

--- apps/users/static/users/js/script.js
+++ apps/users/static/users/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   // Create a Stripe client
-  var stripe = Stripe('pk_live_GgOomyKy2RnxX7npj2YZV0XR');
+  var stripe = Stripe('pk_test_QvqFPIH6h4uZRvIGHSGAQaKB');
 
   // Create an instance of Elements
   var elements = stripe.elements();
