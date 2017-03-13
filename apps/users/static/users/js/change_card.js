ef04a562cf1efec9f2d4f4d671ce1ffb94b3e2e0
zer8ne <jimothy@gmail.com>
2017-03-12 20:04:16 -0700

    Update change_card.js

--- apps/users/static/users/js/change_card.js
+++ apps/users/static/users/js/change_card.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   // Create a Stripe client
-  var stripe = Stripe('pk_live_GgOomyKy2RnxX7npj2YZV0XR');
-  // pk_test_QvqFPIH6h4uZRvIGHSGAQaKB TEST KEY
+  var stripe = Stripe('pk_test_QvqFPIH6h4uZRvIGHSGAQaKB');
   // Create an instance of Elements
   var elements = stripe.elements();
 
